91ce7d50fb340561f7c761f9daae4532bcda8f84
ELJK <y.zhangarin@gmail.com>
2018-02-12 00:13:12 +0000

    Test Commit

---
+++ src/main/com/tamila/myapp/Tamila.java
@@ -0,0 +1,13 @@
+package com.tamila.myapp;
+
+public class Tamila {
+
+    public static void main(String[] args) {
+        printToConsole();
+    }
+
+    public static boolean printToConsole() {
+        System.out.println("Hello Tamila!");
+        return true;
+    }
+}
---
+++ src/test/com/tamila/myapp/TamilaTest.java
@@ -0,0 +1,12 @@
+package com.tamila.myapp;
+
+/**
+ * Created by ELJK on 11/02/2018.
+ */
+public class TamilaTest {
+
+  public static void main(String[] args) {
+    assert Tamila.printToConsole() == true;
+  }
+
+}
